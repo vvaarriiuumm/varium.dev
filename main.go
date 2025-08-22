53fabce35350d0dc76a020a240f6df24a32e7bd0
vvaarriiuumm <rcstouro123@gmail.com>
2025-08-22 09:47:29 -0500

    v1.0.0

--- main.go
+++ main.go
@@ -3,27 +3,52 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
+
+	"database/sql"
+	"html/template"
+
+	_ "modernc.org/sqlite" // We have to put a _ because its a driver and will be detected as unused by compiler
 
 	"github.com/joho/godotenv"
 )
 
+var templates *template.Template
+
+func init() {
+	fmt.Println("Initializing...")
+	templates = template.Must(template.ParseGlob("static/html/*.html"))
+
+	// Create tables in database
+	db, err := sql.Open("sqlite", "database.db")
+	if err != nil {
+		fmt.Println("Error opening database: ", err)
+	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS avatar (link TEXT)")
+
+	if err != nil {
+		fmt.Println("Error creating avatar table in database: ", err)
+	}
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 
-	file, err := os.Open("static/html/index.html")
+	err := templates.ExecuteTemplate(w, "index.html", nil)
+
 	if err != nil {
-		fmt.Println("Error opening HTML file: ", err)
-	}
-	_, err = io.Copy(w, file)
-	if err != nil {
-		fmt.Println("Error copying file to http response writer: ", err)
+		fmt.Println("Error executing template: ", err)
 	}
 
 }
 
-func apiAvatar(w http.ResponseWriter, r *http.Request) {
+func updateAvatar(link string) {
+
+	// This will take in the avatar link from the database and check if
+	// the avatar has changed using the Discord API. If it has
+	// then we update it in the database.
+
 	req, err := http.NewRequest("GET", "https://discord.com/api/users/@me", nil)
 	if err != nil {
 		fmt.Println("Error building request: ", err)
@@ -36,9 +61,7 @@ func apiAvatar(w http.ResponseWriter, r *http.Request) {
 	}
 
 	discord_api_token := os.Getenv("DISCORD_API_TOKEN")
-	fmt.Println(discord_api_token)
 	req.Header.Set("Authorization", "Bearer "+discord_api_token)
-	fmt.Println(req)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -54,8 +77,93 @@ func apiAvatar(w http.ResponseWriter, r *http.Request) {
 
 	var user User
 	json.NewDecoder(resp.Body).Decode(&user)
+	avatarLink := "https://cdn.discordapp.com/avatars/" + user.Id + "/" + user.Avatar + ".png"
 
-	fmt.Fprintf(w, "%s", "https://cdn.discordapp.com/avatars/"+user.Id+"/"+user.Avatar+".png")
+	fmt.Println("avatarLink: ", avatarLink+" | link: ", link)
+
+	if avatarLink != link {
+		db, err := sql.Open("sqlite", "database.db")
+		if err != nil {
+			fmt.Println("Error opening database: ", err)
+		}
+		fmt.Println("Setting avatar link to: ", avatarLink)
+		_, err = db.Exec("INSERT OR REPLACE INTO avatar (link) VALUES (?)", avatarLink)
+		if err != nil {
+			fmt.Println("Error updating the avatar in database: ", err)
+		}
+
+		err = db.Close()
+
+		if err != nil {
+			fmt.Println("Error closing database: ", err)
+		}
+	}
+
+}
+
+func apiAvatar(w http.ResponseWriter, r *http.Request) {
+
+	// We need to return the link from the database, but then pass it off to
+	// updateAvatar to see if its changed
+
+	// 1. Fetch link from database
+	// 2. Return it
+
+	var recheck bool
+	recheck = true
+
+	db, err := sql.Open("sqlite", "database.db")
+
+	if err != nil {
+		fmt.Println("Error opening database: ", err)
+	}
+
+	// Select link from database
+	rows, err := db.Query("SELECT link FROM avatar")
+
+	if err != nil {
+		fmt.Println("Error fetching avatar link from database: ", err)
+	}
+
+	var avatarFromDB string
+	rows.Next()
+	err = rows.Scan(&avatarFromDB)
+	if err != nil {
+		fmt.Println("Error scanning rows: ", err)
+	}
+
+	if avatarFromDB == "" {
+		fmt.Println("avatarFromDB is NULL")
+		// Update the avatar to fill the database with a valid URL
+		// None as the string so that we always generate a new URL
+		updateAvatar("None")
+
+		// Refetch the avatar from the database, should be a valid URL.
+		rows, err = db.Query("SELECT link FROM avatar")
+
+		if err != nil {
+			fmt.Println("Error fetching avatar link from database: ", err)
+		}
+
+		recheck = false
+	}
+
+	rows.Next()
+	err = rows.Scan(&avatarFromDB)
+
+	if err != nil {
+		fmt.Println("Error scanning rows: ", err)
+	}
+
+	fmt.Println("avatarFromDB: ", avatarFromDB)
+
+	// We use goroutine here so that we don't have to wait for completion
+	// Update the avatar using the link from the database to see if it has changed
+	if recheck {
+		go updateAvatar(avatarFromDB)
+	}
+
+	fmt.Fprintf(w, "%s", avatarFromDB)
 
 }
 
--- scripts/discord_token.py
+++ scripts/discord_token.py
@@ -9,8 +9,9 @@ def exchange_code():
     API_ENDPOINT = 'https://discord.com/api/v10'
     CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
     CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET")
+    DISCORD_CODE = os.getenv("DISCORD_CODE")
     REDIRECT_URI = "http://127.0.0.1:8000"
-    DISCORD_CODE = "ixCfi4e6LVXReOp3va5Y9iiCF6tCm0"
+
 
     print("CLIENT_ID: ", CLIENT_ID)
     print("CLIENT SECRET: ", CLIENT_SECRET)
@@ -18,12 +19,12 @@ def exchange_code():
 
 
     data = {
-    'grant_type': 'authorization_code',
-    'code': DISCORD_CODE,
-    'redirect_uri': REDIRECT_URI
+        'grant_type': 'authorization_code',
+        'code': DISCORD_CODE,
+        'redirect_uri': REDIRECT_URI
     }
     headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
     r = requests.post('%s/oauth2/token' % API_ENDPOINT, data=data, headers=headers, auth=(CLIENT_ID, CLIENT_SECRET))
     r.raise_for_status()
