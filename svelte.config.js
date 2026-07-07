14a071c109cbcfde90e241cf53b489dd28057495
var <variumm@proton.me>
2026-07-07 03:02:43 -0500

    Update for wrangler/cloudflare

---
+++ svelte.config.js
@@ -0,0 +1,9 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: { adapter: adapter()// See below for an explanation of these options
+	 }
+};
+
+export default config;
